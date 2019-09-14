568e3a699cc5757dce4f2a03ca97065f36bdac18
akashsingh96 <akash96317@gmail.com>
2019-09-14 20:24:37 +0530

    Navbar state

--- app/component/popular.js
+++ app/component/popular.js
@@ -1,13 +1,37 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
   render() {
     const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
     return (
       <ul className="flex-center">
         {languages.map(language => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+              className="btn-clear nav-link"
+              onClick={() => this.handleClick(language)}
+              style={
+                language === this.state.selectedLanguage
+                  ? { color: "rgb(187, 47, 31) " }
+                  : null
+              }
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
